a56706b96afe16367593dbf40c16bd4ed22c940e
Daria <gritsaenkoda@gmail.com>
2022-02-01 01:12:36 +0300

    Проектная 5

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let popupClose = document.querySelector('.popup__close-button');
-let popupName = document.querySelector('.popup__input_type_name');
-let popupDescription = document.querySelector('.popup__input_type_description');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let popupForm = document.querySelector('.popup__form');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup__edit');
+const popupAdd = document.querySelector('.popup__add');
+
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const popupClose = document.querySelector('.popup__close-button');
+const popupName = document.querySelector('.popup__input_type_name');
+const popupDescription = document.querySelector('.popup__input_type_description');
+
+const popupForm = document.querySelector('.popup__form');
+
+
 
 function openPopup() {
-  popupName.value = profileTitle.textContent;
-  popupDescription.value = profileSubtitle.textContent;
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
+
+function openPopupEdit() {
+  popupName.value = profileTitle.textContent;
+  popupDescription.value = profileSubtitle.textContent;
+  popupEdit.classList.add('popup__edit_active');
+}
+
+function closePopupEdit() {
+  popupEdit.classList.remove('popup__edit_active');
+}
+
+function openPopupAdd() {
+  popupAdd.classList.add('popup__add_active');
+}
+
+function closePopupAdd() {
+  popupAdd.classList.remove('popup__add_active');
+}
+
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -26,6 +52,20 @@ function formSubmitHandler(evt) {
   closePopup();
 }
 
-editButton.addEventListener('click', openPopup);
-popupClose.addEventListener('click', closePopup);
+editButton.addEventListener('click', () => {
+  openPopup();
+  openPopupEdit();
+});
+addButton.addEventListener('click', () => {
+  openPopup();
+  openPopupAdd();
+});
+
+popupClose.addEventListener('click', () => {
+  closePopup();
+  closePopupAdd();
+  closePopupEdit();
+});
+
+
 popupForm.addEventListener('submit', formSubmitHandler);
